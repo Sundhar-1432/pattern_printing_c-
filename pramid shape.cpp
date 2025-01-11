80915199cd93111e15bfb470458282ce86ad0b8c
SUNDHAR V <SVSsundhar143@gmail.com>
2025-01-12 00:57:45 +0530

    [space,star,space] 3inner loops
          *      
         ***     
        *****    
       *******   
      *********

---
+++ pramid shape.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int pattern(int n){
+    for(int i=0;i<=n;i++){
+        //space
+        for(int j=0;j<=n-i-1;j++){
+            cout<<" ";
+        }
+        //star
+        for(int k=0;k<2*i+1;k++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0;j<=n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+int main() {
+    int n;
+    cout<<"enter your number of rows:";
+    cin>>n;
+    pattern(n);
+    return 0;
+}
